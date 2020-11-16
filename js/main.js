ca7580b9b1e22e7ad854f4fbcb546d1fcddbf720
Enzo Valfort <60793213+Redexes-web@users.noreply.github.com>
2020-11-16 18:39:03 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -32,3 +32,47 @@ if (majeur) {
 else{
     alert("vous êtes mineur")
 }
+
+
+// exercice 4
+
+function isPrime(num) {
+    for(var i = 2; i < num; i++)
+      if(num % i === 0) return false;
+    return num > 1;
+  }
+  document.write("<br> <h2>nombres premiers:</h2>  <br> <p>")
+  let primeNumbers = [], i=1;
+  do{
+    if(isPrime(i)){
+        primeNumbers.push(i);
+        document.write(i + ", ")
+    }
+    i++;
+  }
+  while (i<100)
+document.write("</p>")
+  
+  //exercice 5
+document.write("<h2> Table de multiplication</h2><table class=\"table\">")
+for (let i = 1; i <= 10; i++) {
+    document.write("<tr>")
+    for (let z = 1; z <= 10; z++) {
+        document.write("<td>" + z*i + "</td>")
+        
+    }
+    document.write("</tr>")
+    
+}
+document.write("</table>")
+
+
+//exercice 6
+
+let btn = document.getElementById("btn");
+btn.onclick = function() {
+    if (document.body.style.background == "darkgray") 
+        document.body.style.background = "white";
+    else
+    document.body.style.background = "darkgray";
+    }; 
