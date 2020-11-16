1049bea2251d715e3e037a8577a4c96e0e0589e8
Enzo Valfort <60793213+Redexes-web@users.noreply.github.com>
2020-11-16 09:48:10 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// exercice 1
+
+alert("bonjour tout le monde");
+let prenom = prompt("quel est votre prénom ?");
+alert("bonjour " + prenom);
+
+nombre1 = parseInt(prompt("veuillez entrer votre premier nombre :"));
+nombre2 = parseInt(prompt("veuillez entrer votre second nombre :"));
+alert("La somme de vos nombre est: " + (nombre1+nombre2));
+age = parseInt(prompt("Entrez votre age: "));
+console.log(typeof age)
+console.log(age)
+
+while (isNaN(age)) {
+    age = parseInt(prompt("Veuillez renseigner un age en chiffre :"));
+}
+majeur = age>=18 ? true : false ;
+
+if (majeur) {
+    alert("Vous êtes majeur");
+}
+else{
+    alert("vous êtes mineur")
+}
