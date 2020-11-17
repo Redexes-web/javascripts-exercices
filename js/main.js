92bd4686a9be18a2e11f5e7b4c7ab7dc6930952b
Enzo Valfort <60793213+Redexes-web@users.noreply.github.com>
2020-11-17 10:53:16 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
-// // exercice 1
+// exercice 1
 
-// alert("bonjour tout le monde");
-// let prenom = prompt("quel est votre prénom ?");
-// alert("bonjour " + prenom);
-
-
-// // exercice 2
-
-// function somme(num1, num2) {
-//     return num1+num2;
-// }
-
-// let nombre1 = parseInt(prompt("veuillez entrer votre premier nombre :")),
-//     nombre2 = parseInt(prompt("veuillez entrer votre second nombre :"));
-// alert("La somme de vos nombre est: " + somme(nombre1,nombre2));
+alert("bonjour tout le monde");
+let prenom = prompt("quel est votre prénom ?");
+alert("bonjour " + prenom);
 
 
-// // exercice 3
+// exercice 2
 
-// let age = parseInt(prompt("Entrez votre age: "));
+function somme(num1, num2) {
+    return num1+num2;
+}
 
-// while (isNaN(age) || age > 123) {
-//     age = parseInt(prompt("Veuillez renseigner un age en chiffre :"));
-// }
+let nombre1 = parseInt(prompt("veuillez entrer votre premier nombre :")),
+    nombre2 = parseInt(prompt("veuillez entrer votre second nombre :"));
+alert("La somme de vos nombre est: " + somme(nombre1,nombre2));
 
-// let majeur = age>=18 ? true : false ;
 
-// if (majeur) {
-//     alert("Vous êtes majeur");
-// }
-// else{
-//     alert("vous êtes mineur")
-// }
+// exercice 3
+
+let age = parseInt(prompt("Entrez votre age: "));
+
+while (isNaN(age) || age > 123) {
+    age = parseInt(prompt("Veuillez renseigner un age en chiffre :"));
+}
+
+let majeur = age>=18 ? true : false ;
+
+if (majeur) {
+    alert("Vous êtes majeur");
+}
+else{
+    alert("vous êtes mineur")
+}
 
 
 // exercice 4
